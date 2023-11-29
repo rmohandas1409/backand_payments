d4e839fe7170e41787646e03703ca7a653375ef8
raphaelmohandas <raphaelmohandaspiresrodrigues@gmail.com>
2023-11-28 23:01:06 -0300

    Acrecentado um campo de celular para utilizar na versão dois da api exemplo quando temos duas aplicações tipo mobile e uma app web.

--- clientes/models.py
+++ clientes/models.py
@@ -6,6 +6,7 @@ class Cliente(models.Model):
     cpf = models.CharField(max_length=11, unique=True, default='')
     rg = models.CharField(max_length=9, default='')
     telefone = models.CharField(max_length=14, default='')
+    celular = models.CharField(max_length=11, default="")
     ativo = models.BooleanField(max_length=1, default=True)
 
     def __str__(self):
